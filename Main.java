90d40c8ad7b585f2ba9d06a3f823ac6936888f52
yashbudd:git config --global user.name yashbuddgit config --global user.email yashbuddhdeo@gmail.com <135936758+yashbudd@users.noreply.github.com>
2025-07-16 16:58:03 +0200

    Added app functionality

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Mass Transit Simulation System! To start, type 'help' for a list of commands.");
         TravelController simulator = new TravelController();
+        Application.launch(Browser.class, args);
         simulator.commandLoop();
     }
 }
